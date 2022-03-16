a71aa72afebb562141a1841e113a86d494fc708a
wallissonlevi <91345847+wallissonlevi@users.noreply.github.com>
2022-03-16 18:29:03 -0300

    Add files via upload

---
+++ Projeto Mata Mosquito/jogo.js
@@ -0,0 +1,117 @@
+// Definindo as dimenções do jogo
+var altura = 0 // As variáveis devem ser declaradas fora do escopo da função para seram alteradas pela função;
+var largura = 0
+var vidas = 1
+var tempo = 10
+var criaMoscaTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+
+if (nivel === 'normal') {
+	criaMoscaTempo = 1500
+} else if(nivel === 'dificil') {
+	criaMoscaTempo = 1000
+} else if(nivel === 'chucknorris') {
+	criaMoscaTempo = 750
+}
+
+function ajustaTamanhoPalcoJogo() {
+	altura = window.innerHeight
+	largura = window.innerWidth
+
+	console.log(largura, altura)
+}
+ajustaTamanhoPalcoJogo() // Aqui a função é chama para que os valores sejam atualzizados. Também é aplixado o oversize no body do documento HTML.
+
+var cronometro =setInterval(function() {
+	tempo -= 1
+	if (tempo < 0) {
+		clearInterval(cronometro)
+		clearInterval(criaMosca)
+		window.location.href = 'vitoria.html'
+	} else {
+
+	}
+
+	document.getElementById('cronometro').innerHTML = tempo
+	
+}, 1000)
+
+
+
+
+function posicaoRandomica() {
+
+		// Remover o elemento anterior (caso exista)
+		if(document.getElementById('mosca')) {
+			document.getElementById('mosca').remove()
+
+			if (vidas > 3) {
+
+				window.location.href = 'fim_jogo.html'
+			} else {
+				
+			}
+
+			document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+			vidas++
+		}
+		
+
+
+		// Definir a posição das moscas de modo randômico
+		var posicaoX = Math.floor(Math.random() * largura) - 90
+		var posicaoY = Math.floor(Math.random() * altura) - 90
+
+		posicaoX = posicaoX < 0 ? 0 : posicaoX
+		posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+		console.log(posicaoX, posicaoY)
+
+		// Criar o elemento html
+		var mosca = document.createElement('img')
+		mosca.src = 'imagens/mosca.png'
+		mosca.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+		mosca.style.left = posicaoX + 'px'
+		mosca.style.top = posicaoY + 'px'
+		mosca.style.position = 'absolute'
+		mosca.id = 'mosca'
+		mosca.onclick = function () {
+			this.remove()
+		}
+
+		document.body.appendChild(mosca)
+		console.log(ladoAleatorio())
+}
+
+
+// Criando tamanhos aleatórios - utilizando lógica baseada em 
+function tamanhoAleatorio() {
+	var classe = Math.floor(Math.random() * 3)
+	
+	switch(classe) {
+		case 0:
+			return 'mosca1'
+		case 1:
+			return 'mosca2'
+		case 2:
+			return 'mosca3'
+
+	}
+}
+
+// Posicionamento aleatório
+
+function ladoAleatorio() {
+	var classe = Math.floor(Math.random() * 2)
+	switch(classe) {
+		case 0:
+			return 'ladoA'
+		case 1:
+			return 'ladoB'
+	}
+}
+
+// Intervalo de tempo da mosca na tela
